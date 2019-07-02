cfe6425c335910a7921883ec4cb1ce4adcc5b54d
morenyangbot <45971234+morenyangbot@users.noreply.github.com>
2019-07-02 16:57:07 +0800

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import model.Student;
+
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student("YT", 18, "male", "123");
+        System.out.println(student.getName());
+        System.out.println(student.getAge());
+        System.out.println(student.getNo());
+        System.out.println(student.getSex());
+    }
+}
---
+++ src/model/Student.java
@@ -0,0 +1,47 @@
+package model;
+
+public class Student {
+    public Student(String name, int age, String sex, String no) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+        this.no = no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo(String no) {
+        this.no = no;
+    }
+
+    private String name;
+    private int age;
+    private String sex;
+    private String no;
+}
